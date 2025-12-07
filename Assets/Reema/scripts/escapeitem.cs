972c35bc0a1182630cf9ee39983da576878a230e
Reoniix <revmwi.a@gmail.com>
2025-12-07 20:07:45 +0300

    ReemaScripts
    edited escapeitem.cs script to destroy items parent
    added FloatingItem.cs fully

--- Assets/Reema/scripts/escapeitem.cs
+++ Assets/Reema/scripts/escapeitem.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class escapeitem : MonoBehaviour, Iinteractable
 {
     [SerializeField] private GameObject Player;
+    private RayInetractor Hit; //bring in an element fromanother script, private (script name) (name in this file)
 
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
-        
+        Destroy(Hit.transform.root.gameObject); //wtvthe ray hits (mesh or collider) the "root" part gets to the parent of the object and destroys it
     }
     public void interact()
     {
