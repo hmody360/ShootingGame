ae9a9285cfeb1b540d9e94b27a61e1fef084e44a
Mohammed Soqati <hmadahmody360@gmail.com>
2025-12-15 02:53:53 +0300

    Added How To Play Guide

--- Assets/Main/Scripts/UI Scripts/MainMenuScenes.cs
+++ Assets/Main/Scripts/UI Scripts/MainMenuScenes.cs
@@ -5,19 +5,22 @@ public class MainMenuScenes : MonoBehaviour
 {
 
     public GameObject mainMenuPage;
+    public GameObject mainMenuOptionsPage;
+    public GameObject GameLogo;
     public GameObject settingsPage;
+    public GameObject guidePage;
 
     void Start()
     {
         ShowMainMenu(); //Show the Main Menu on Start
-
-
     }
 
     public void ShowMainMenu() //Show Main Menu
     {
         mainMenuPage.SetActive(true);
+        GameLogo.SetActive(true);
         settingsPage.SetActive(false);
+        guidePage.SetActive(false);
     }
     public void StartGame() //Start the game scene
     {
@@ -26,12 +29,30 @@ public class MainMenuScenes : MonoBehaviour
 
     public void OpenSettings() //Open Settings Panel
     {
+        mainMenuOptionsPage.SetActive(false);
+        GameLogo.SetActive(false);
         settingsPage.SetActive(true);
     }
 
     public void CloseSettings() //Close Settings Panel
     {
+        mainMenuOptionsPage.SetActive(true);
+        GameLogo.SetActive(true);
         settingsPage.SetActive(false);
+    }
+
+    public void OpenGuide() //Open How to Play Panel
+    {
+        mainMenuOptionsPage.SetActive(false);
+        GameLogo.SetActive(false);
+        guidePage.SetActive(true);
+    }
+
+    public void CloseGuide() //Close How to Play Panel
+    {
+        mainMenuOptionsPage.SetActive(true);
+        GameLogo.SetActive(true);
+        guidePage.SetActive(false);
     }
 
     public void ExitGame() //Close the game
